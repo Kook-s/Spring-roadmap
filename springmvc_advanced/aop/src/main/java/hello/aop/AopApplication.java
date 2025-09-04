f41606b30e817c9e99b66c666865e84c74cfb157
Kook-s <kooks1126@naver.com>
2025-09-04 16:21:07 +0900

    aop internal external

--- springmvc_advanced/aop/src/main/java/hello/aop/AopApplication.java
+++ springmvc_advanced/aop/src/main/java/hello/aop/AopApplication.java
@@ -1,7 +1,10 @@
 package hello.aop;
 
+import hello.aop.exam.aop.RetryAspect;
+import hello.aop.exam.aop.TraceAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class AopApplication {
